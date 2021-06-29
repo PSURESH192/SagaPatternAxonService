af9b3e26e8b05e23afbf1579aa8837f3253da00d
Suresh_Prabha <suresh.uday92@gmail.com>
2021-06-29 18:16:11 +0530

    Tests

--- hotel-service/src/test/java/com/project/hotelmanagement/hotelservice/HotelServiceApplicationTests.java
+++ hotel-service/src/test/java/com/project/hotelmanagement/hotelservice/HotelServiceApplicationTests.java
@@ -1,16 +1,41 @@
 package com.project.hotelmanagement.hotelservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.project.commands.CreateHotelCommand;
+import com.project.commands.UpdateHotelStatusCommand;
+import com.project.events.HotelCreatedEvent;
+import com.project.events.HotelStatusUpdatedEvent;
+import com.project.hotelmanagement.hotelservice.aggregates.HotelAggregate;
+import org.axonframework.test.aggregate.AggregateTestFixture;
+import org.axonframework.test.aggregate.FixtureConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import java.util.UUID;
+
 @SpringBootTest
 public class HotelServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	private FixtureConfiguration<HotelAggregate> fixture;
+
+	@BeforeEach
+	public void setUp() {
+		fixture = new AggregateTestFixture<>(HotelAggregate.class);
 	}
 
+	@Test
+	void testCreateHotelCommandEventHandler(){
+		String hotelId = UUID.randomUUID().toString();
+		fixture.givenNoPriorActivity()
+				.when(new CreateHotelCommand(hotelId,"RESORT", 1000L,"INR","CREATED"))
+				.expectEvents(new HotelCreatedEvent(hotelId,"RESORT", 1000L,"INR","CREATED"));
+	}
+
+	@Test
+	void testUpdateHotelStatusCommandEventHandler(){
+		String hotelId = UUID.randomUUID().toString();
+		fixture.given(new HotelCreatedEvent(hotelId,"RESORT", 1000L,"INR","CREATED"))
+				.when(new UpdateHotelStatusCommand(hotelId,"CREATED"))
+				.expectEvents(new HotelStatusUpdatedEvent(hotelId,"CREATED"));
+	}
 }
--- payment-service/src/test/java/com/project/paymentmanagement/paymentservice/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/project/paymentmanagement/paymentservice/PaymentServiceApplicationTests.java
@@ -1,16 +1,32 @@
 package com.project.paymentmanagement.paymentservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.project.commands.CreateInvoiceCommand;
+import com.project.events.InvoiceCreatedEvent;
+import com.project.paymentmanagement.paymentservice.aggregates.PaymentAggregate;
+import org.axonframework.test.aggregate.AggregateTestFixture;
+import org.axonframework.test.aggregate.FixtureConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import java.util.UUID;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class PaymentServiceApplicationTests {
 
+	private FixtureConfiguration<PaymentAggregate> fixture;
+
+	@BeforeEach
+	public void setUp() {
+		fixture = new AggregateTestFixture<>(PaymentAggregate.class);
+	}
+
 	@Test
-	public void contextLoads() {
+	void testCreateInvoiceCommandEventHandler(){
+		String hotelId = UUID.randomUUID().toString();
+		String paymentId = UUID.randomUUID().toString();
+		fixture.givenNoPriorActivity()
+				.when(new CreateInvoiceCommand(paymentId,hotelId))
+				.expectEvents(new InvoiceCreatedEvent(paymentId,hotelId));
 	}
 
 }
